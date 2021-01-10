4cd38b9474320aaf622b438af26099a54e7218e1
Tarun Bharti <tarun@backbase.com>
2021-01-10 20:56:16 +0100

    Update CardsMappers.java

--- src/main/java/com/backbase/productled/mapper/CardsMappers.java
+++ src/main/java/com/backbase/productled/mapper/CardsMappers.java
@@ -37,7 +37,7 @@ public interface CardsMappers {
     @Mapping(target = "subType", expression = "java(cardResponse.getMetadata().get(\"subType\"))")
     @Mapping(target = "holder", expression = "java(new com.backbase.presentation.card.rest.spec.v2.cards.CardHolder().name(cardResponse.getMetadata().get(\"cardHolderName\")))")
     @Mapping(target = "name", expression = "java(cardResponse.getMetadata().get(\"name\"))")
-    @Mapping(target = "status", expression = "java(cardResponse.getState() == com.backbase.marqeta.clients.model.CardResponse.StateEnum.ACTIVE || com.backbase.marqeta.clients.model.CardResponse.StateEnum.SUSPENDED  ? \"Active\" : \"Inactive\")")
+    @Mapping(target = "status", expression = "java(cardResponse.getState() == com.backbase.marqeta.clients.model.CardResponse.StateEnum.ACTIVE || cardResponse.getState() == com.backbase.marqeta.clients.model.CardResponse.StateEnum.SUSPENDED  ? \"Active\" : \"Inactive\")")
     @Mapping(target = "lockStatus", expression = "java(com.backbase.presentation.card.rest.spec.v2.cards.LockStatus.fromValue(cardResponse.getMetadata().get(\"lockStatus\")))")
     @Mapping(target = "replacement", expression = "java(new com.backbase.presentation.card.rest.spec.v2.cards.Replacement().status(cardResponse.getMetadata().get(\"replacementStatus\")).reason(cardResponse.getMetadata().get(\"replacementReason\")))")
     @Mapping(target = "expiryDate", expression = "java(new com.backbase.presentation.card.rest.spec.v2.cards.YearMonth().year(String.valueOf(cardResponse.getExpirationTime().getYear())).month(String.valueOf(cardResponse.getExpirationTime().getMonthValue())))")
